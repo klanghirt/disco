aa994719fb0e54165cce3de9b03d23357c4df26b
klanghirt <116285798+klanghirt@users.noreply.github.com>
2022-11-01 09:07:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,7 +47,7 @@ function draw() {
     textAlign(CENTER);
   textFont("trebuchet MS");
     textSize(50);
-   text ("FLASH WARNING", width / 2 + 5, 245)
+   text ("FLASH WARNING", width / 2, 245)
   }
   
   
