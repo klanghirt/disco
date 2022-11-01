ff4bfad484b53e2c8234990f12d1bf708700cd4d
klanghirt <116285798+klanghirt@users.noreply.github.com>
2022-11-01 09:05:25 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,62 +1,68 @@
 let r, g, b; 
+var wait;
+
+let disco = false;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  time = millis();
   colorMode(RGB);
   slider = createSlider(0, 151, 0);
-  slider.position(width / 2, height / 1.03);
   slider.style("width", "300px");
   noFill();
+  
+  button = createButton("Click to Continue");
+  button.mousePressed(() => disco = true);
+  button.style("background-color", "200")
+  background(255);
+  
+  //slider can't be customized. I've had problems with it since the beginning of this unit. the color is always green, regardless of what files I open. I copy/pasted the code in the html file from w3Schools and it works for the button, but not the slider. very lost and confused
+  slider.style("background-color", "pink")
 }
 
 function draw() {
   background(0);
  
   let val = slider.value();
-  let x = random (255);
+  let wait = 200
   
-  push();
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      stroke(1.2 * val);
-      strokeWeight(2);
-      let gapW = width * 0.2;
-      let gapH = height * 0.1;
-      let ix = width * 0.1;
-      let iy = height * 0.1;
-      let x = ix + gapW * i;
-      let y = iy + gapH * j;
-      strokeWeight(4);
-      circle (x, y, 3 * val);
-      strokeWeight(3);
-      circle(x, y, 2 * val);
-      strokeWeight(2);
-      circle(x, y, val);
-      strokeWeight(1);
-      circle(x, y, val/2)
-    }
-  }
-  pop();
+  button.position(windowWidth / 2 - 58, windowHeight / 2 - 2);
+  slider.position(width / 2 - 150, height / 1.2);
   
-  push();
-  translate(width / 2, height / 2);
-  noStroke();
-  fill(r, g, b);
-  rotate(val / 2);
-  textSize(val * 3);
-  textFont("blenny");
-  textAlign(CENTER, CENTER);
-  text("disco", 5, 10);
-  pop();
-  
+  if (disco) {
+    slider.style ('display', 'block');
+    button.style ('display', 'none');
+  if ((millis() - time) >= wait) {
+    fill(r, g, b);
+    time = millis();
   if (val > 0){
    r = random (255)
    g = random (255)
    b = random (255)
-   stroke(x)
-    
+ }
   }
- 
-
+ } else {
+   slider.style ('display', 'none');
+   fill (255);
+    textAlign(CENTER);
+  textFont("trebuchet MS");
+    textSize(50);
+   text ("FLASH WARNING", width / 2 + 5, 245)
+  }
+  
+  
+  translate(width / 2, height / 2);
+  noStroke();
+  fill(r, g, b);
+  rotate(val / 2);
+  textSize(val * 2.5);
+  textFont("blenny");
+  textAlign(CENTER, CENTER);
+  text("disco", 5, 10);
+  
+  
+  
+  
 }
 
 function windowResized() {
